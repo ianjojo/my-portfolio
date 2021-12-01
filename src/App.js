56bed312e024699d9eca972f28eaba2d21701a47
Ian Johnston <ianallanjohnston@gmail.com>
2021-11-30 20:18:39 -0800

    commenting out bits

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+// import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
-import About from "./components/About";
-import SinglePost from "./components/SinglePost";
-import Post from "./components/Post";
+// import About from "./components/About";
+// import SinglePost from "./components/SinglePost";
+// import Post from "./components/Post";
 import Project from "./components/Project";
 
-import NavBar2 from "./components/Nav/Navbar";
+// import NavBar2 from "./components/Nav/Navbar";
 // import Newnav from "./components/newnav/newnav";
-import Accordion from "./components/accordian/Accordian";
+// import Accordion from "./components/accordian/Accordian";
 import "./typography.scss";
 import "./App.scss";
-import StickyNav from "./components/sticky/Accordian";
+// import StickyNav from "./components/sticky/Accordian";
 import SimpleNav from "./components/simplenav/Navbar";
 function App() {
   return (
     <div className="container">
-      {/* <BrowserRouter> */}
-      {/* <StickyNav /> */}
-      {/* <Accordion /> */}
-      {/* <NavBar2 /> */}
       <SimpleNav />
       <Home />
       <Project />
